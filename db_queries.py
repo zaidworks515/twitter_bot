d7a5a1e7dca5cef00b1b64b636bd702e5e74036d
Zaid Ahmed <zaid_works515@outlook.com>
2025-02-14 17:33:20 +0500

    selected_accounts added, convo chain added, db structure updated

--- db_queries.py
+++ db_queries.py
@@ -19,16 +19,16 @@ def create_connection():
         return None
     
     
-def insert_results(tagged_tweet_id=None, author_id=' ', tagged_tweet=None, replied_comments=None, post_status=None):   
+def insert_results(tagged_tweet_id=None, author_id=' ', tagged_tweet=None, replied_comments=None, post_status=None, conversation_id=None):   
     connection = create_connection()
     if connection:
         cursor = connection.cursor()
         try:    
-            insert_query = """INSERT INTO tweet_record (tagged_tweet_id, author_id, tagged_tweet, replied_comments, post_status) 
-            VALUES (%s, %s, %s, %s, %s)"""
-            cursor.execute(insert_query, (tagged_tweet_id, author_id, tagged_tweet, replied_comments, post_status))
+            insert_query = """INSERT INTO tweet_record (tagged_tweet_id, author_id, tagged_tweet, replied_comments, post_status, conversation_id) 
+            VALUES (%s, %s, %s, %s, %s, %s)"""
+            cursor.execute(insert_query, (tagged_tweet_id, author_id, tagged_tweet, replied_comments, post_status, conversation_id))
             connection.commit()
-            return cursor.lastrowid or 'can not posted a tweet.' # Yeh created id return krega
+            return cursor.lastrowid or 'can not save posted a tweet.' # Yeh created id return krega
         except Error as e:
             connection.rollback()
             return f"An error occurred: {e}"
@@ -38,22 +38,39 @@ def insert_results(tagged_tweet_id=None, author_id=' ', tagged_tweet=None, repli
     else:
         return "Unable to connect to the database"
 
-
     
-def check_status(tagged_tweet_id):
+def check_status(tagged_tweet_id, conversation_id, author_id):
     connection = create_connection()
     if connection:
         cursor = connection.cursor()
         try:
-            cursor.execute("SELECT * FROM tweet_record WHERE tagged_tweet_id = %s", (tagged_tweet_id,))
-            
+            cursor.execute("SELECT * FROM tweet_record WHERE tagged_tweet_id = %s", (tagged_tweet_id))
             results = cursor.fetchall()
+
+            cursor.execute("SELECT COUNT(*) FROM tweet_record WHERE conversation_id = %s AND author_id = %s", (conversation_id, author_id))
+            reply_result = cursor.fetchone()
+
+            reply_count = reply_result[0]
+            print(reply_count)
+
+            is_reply = "True" if reply_result[0] > 0 else "False"
+            print("reply_status ", is_reply)
             
+
+            conversation_chain = []
+            if is_reply == "True":
+                cursor.execute("SELECT author_id, tagged_tweet FROM tweet_record WHERE conversation_id = %s AND author_id = %s ORDER BY id ASC", (conversation_id, author_id))
+                all_replies = cursor.fetchall()
+
+                for reply in all_replies:
+                    conversation_chain.append({"User": reply[1], "AI_Response": reply[2]})
+
             if results:
-                post_status = results[-1][-1]
-                return post_status
+                post_status = results[-1][0]  # Get the latest post_status
+                print("Post status:", post_status)
+                return post_status, is_reply, reply_count, conversation_chain
             else:
-                return None  
+                return None, is_reply, reply_count, conversation_chain
             
         except Error as e:
             return f"An error occurred: {e}"  
@@ -62,7 +79,6 @@ def check_status(tagged_tweet_id):
             connection.close()
     else:
         return "Unable to connect to the database"
-    
 
 
 def check_tweets(tweet_category, from_date, to_date):
