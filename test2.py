2b8aa7f61b409b67923aea5cef9ca84b3c85e910
Zaid Ahmed <zaid_works515@outlook.com>
2025-03-10 16:42:19 +0500

    .

--- test2.py
+++ test2.py
@@ -1,92 +1,13 @@
-# from twitter_functions import fetch_tagged_tweets
-# import pytz
-# from datetime import datetime, timedelta
-# import json
+from twitter_functions import get_news
 
-# netherlands_tz = pytz.timezone("Europe/Amsterdam")
+data = get_news(last_category='AI')
+article = None
 
-# now = datetime.now(netherlands_tz)  
-
-# end_time = now.strftime('%Y-%m-%dT%H:%M:%SZ')
-
-# interval = now - timedelta(hours=56)  
-# start_time = interval.strftime('%Y-%m-%dT%H:%M:%SZ')
+if data:
+    article = data[0]
+    article_category = data[1]
+    print(article)
 
 
-# response = fetch_tagged_tweets(username='game5ball', start_time=start_time, end_time=end_time)
-
-# if response:  
-#     with open('fetch_tagged_tweets.json', 'w') as file:
-#         json.dump(response, file, indent=4)
-#     print("Data stored successfully...")
-# else:
-#     print("No data received.")
-    
-    
-    
-
-
-
-import json 
-from db_queries import insert_results, check_status
-from twitter_functions import get_gork_response
-
-with open('fetch_tagged_tweets2.json', 'r') as file:
-    data = json.load(file)
-
-
-for row in reversed(data['data']):
-    author_id = row['author_id']
-    tweet_id = row['id']
-    tweet_text = row['text']
-    conversation_id = row['conversation_id'] 
-    
-    
-    
-    status, is_reply, reply_count, previous_reply = check_status(tweet_id, conversation_id, author_id)
-    # previous_reply = previous_reply[::-1]
-    print(f"CHAIN::: {previous_reply}")
-
-    reply_text = get_gork_response(tweet_text, is_reply, reply_count, previous_reply)
-    # print(tweet_text)
-    # print(reply_text)
-    print('='*20)
-    
-    print('Comment Successful..........')
-    id = insert_results(tagged_tweet_id=tweet_id, 
-                        author_id=author_id, 
-                        tagged_tweet=tweet_text, 
-                        replied_comments=reply_text,
-                        conversation_id=conversation_id,
-                        post_status='successful')
-    print(f'data saved against ID: {id}')
-    
-    print('='*20)
-    
-    
-
-
-
-# with open('fetch_tagged_tweets.json', 'r') as file:
-#     data = json.load(file)
-
-# chain = []
-# for row in data['data']:
-#     author_id = row['author_id']
-#     tweet_id = row['id']
-#     tweet_text = row['text']
-#     conversation_id = row['conversation_id'] 
-    
-#     status, is_reply, reply_count, previous_reply = check_status(tweet_id, conversation_id, author_id)
-    
-#     previous_reply = previous_reply[::-1]
-#     chain.append(previous_reply)
-    
-
-
-# if chain:  
-#     with open('fetch_chain.json', 'w') as file:
-#         json.dump(chain, file, indent=4)
-#     print("Data stored successfully...")
-# else:
-#     print("No data received.")
+if not article:
+    print("No articles found for the given category.")
--- twitter_functions.py
+++ twitter_functions.py
@@ -8,7 +8,6 @@ from db_queries import check_status, insert_results, check_tweets, insert_result
 from slang_picker import SlangPicker
 import re
 from sentence_transformers import SentenceTransformer, util
-import time
 
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
@@ -699,7 +698,6 @@ def get_news(last_category):
     for _ in range(max_attempts):
         query = categories[index]
         print(f"FETCHING NEWS FOR:: {query}")
-        time.sleep(5)
         url = f"https://gnews.io/api/v4/search?q={query.replace(' ', '%20')}&lang=en&country=us&max=1&apikey={news_api}"
 
         try:
@@ -717,13 +715,7 @@ def get_news(last_category):
                     yesterday = today - timedelta(days=1)
 
                     if article_date >= yesterday:
-                        print("=====" * 10)
-                        print("NEWS ARTICLE::::")
-                        print(articles)
-                        print("=====" * 10)
-                        return articles, query
-            else:
-                print(f"Failed to fetch news for {query}. Status code: {response.status_code}")
+                        return articles, query  
 
             index = (index + 1) % len(categories)
 
